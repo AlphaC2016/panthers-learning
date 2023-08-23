57f2ca845e97baba079b0992895fc768f5367412
btrap <btrap570@gmail.com>
2023-08-23 19:44:20 +0300

    message

--- word api.py
+++ word api.py
@@ -14,9 +14,14 @@ from deep_translator import GoogleTranslator
 import requests
 import json
 
-# Shaked 11111
 FILENAME = r"C:\Users\kelle\Desktop\TESTW.docx"
-PRIVATE_KEY = r"C:\Users\kelle\Desktop\oracel_keys\kellerg2002@gmail.com_2023-08-23T11_46_39.769Z.pem"
+CONFIG_FILE = r"C:\Users\kelle\.oci\config"
+USER = "ocid1.user.oc1..aaaaaaaa7mxia3xroif3st7xwoglkyt3cu24h3zohcqpvpc4oo6lrpnmvuzq"
+FINGERPRINT = "a7:e6:99:17:39:c9:e4:17:bb:42:f2:81:1c:d7:84:af"
+TENANCY = "ocid1.tenancy.oc1..aaaaaaaa7l5viilzwmlk6gmxfyjiektpyusskdbvlwi6bzaqlvxsxy7q2ryq"
+REGION = "il-jerusalem-1"
+KEY_FILE = r"C:\Users\kelle\Desktop\oracel_keys\kellerg2002@gmail.com_2023-08-23T11_46_39.769Z.pem"
+
 def read_docx():
     # Reads the docx file and returns it
     print("enter your word directory:\n")
@@ -31,13 +36,13 @@ def read_docx():
 #     # Text Translation
 #
 #     # Create a default config using DEFAULT profile in default location
-#     # config = oci.config.from_file(r"C:\Users\kelle\.oci\config")
+#     # config = oci.config.from_file(f"{CONFIG_FILE}")
 #     config = {
-#         "user" : "ocid1.user.oc1..aaaaaaaa7mxia3xroif3st7xwoglkyt3cu24h3zohcqpvpc4oo6lrpnmvuzq",
-#     "fingerprint" : "a7:e6:99:17:39:c9:e4:17:bb:42:f2:81:1c:d7:84:af",
-#     "tenancy" : "ocid1.tenancy.oc1..aaaaaaaa7l5viilzwmlk6gmxfyjiektpyusskdbvlwi6bzaqlvxsxy7q2ryq",
-#     "region" : "il-jerusalem-1",
-#     "key_file" : r"C:\Users\kelle\Desktop\oracel_keys\kellerg2002@gmail.com_2023-08-23T11_46_39.769Z.pem"
+#         "user": f"{USER}",
+#         "fingerprint": f"{FINGERPRINT}",
+#         "tenancy": f"{TENANCY}",
+#         "region": f"{REGION}",
+#         "key_file": f"{KEY_FILE}"
 #     }
 #     validate_config(config)
 #     # Initialize service client with default config file
@@ -54,20 +59,21 @@ def read_docx():
 #
 #     # Get the data from response
 #     print(batch_detect_language_key_phrases_response.data)
-def api_ai(translated):
-    # Text Translation
 
+def create_config():
     # Create a default config using DEFAULT profile in default location
-    # config = oci.config.from_file(r"C:\Users\kelle\.oci\config")
+    # config = oci.config.from_file(f"{CONFIG_FILE}")
     config = {
-        "user": "ocid1.user.oc1..aaaaaaaa7mxia3xroif3st7xwoglkyt3cu24h3zohcqpvpc4oo6lrpnmvuzq",
-        "fingerprint": "a7:e6:99:17:39:c9:e4:17:bb:42:f2:81:1c:d7:84:af",
-        "tenancy": "ocid1.tenancy.oc1..aaaaaaaa7l5viilzwmlk6gmxfyjiektpyusskdbvlwi6bzaqlvxsxy7q2ryq",
-        "region": "il-jerusalem-1",
-        "key_file": f"{PRIVATE_KEY}"
+        "user": f"{USER}",
+        "fingerprint": f"{FINGERPRINT}",
+        "tenancy": f"{TENANCY}",
+        "region": f"{REGION}",
+        "key_file": f"{KEY_FILE}"
     }
     validate_config(config)
-    # Initialize service client with default config file
+
+def api_ai(translated):
+    # Get the main key phrases
     ai_language_client = oci.ai_language.AIServiceLanguageClient(config)
 
     batch_detect_language_key_phrases_response = ai_language_client.batch_detect_language_key_phrases(
@@ -115,6 +121,7 @@ def main():
     fulltext = read_docx()
     print(fulltext)
     translated = google_api_trunslet(fulltext)
+    create_config()
     api_ai(translated)
     translated = google_api_trunslet_revers(translated)
 
