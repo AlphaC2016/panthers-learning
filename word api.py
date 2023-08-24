02476bcc02cb523cfb6ed9736c759b82e73ee423
btrap <btrap570@gmail.com>
2023-08-24 05:48:25 +0300

    last change?

--- word api.py
+++ word api.py
@@ -1,7 +1,7 @@
-# ICMP_redirection.py
+# word api.py
 #
-# Programmer  : Guy, shaked
-# Date        : 03/05/2022
+# Programmer  : Guy, Shaked
+# Date        : 03-04/05/2022
 #
 # word api
 # ------------------------------------------------------------------------------------------------------
@@ -24,6 +24,8 @@ FINGERPRINT = "a7:e6:99:17:39:c9:e4:17:bb:42:f2:81:1c:d7:84:af"
 TENANCY = "ocid1.tenancy.oc1..aaaaaaaa7l5viilzwmlk6gmxfyjiektpyusskdbvlwi6bzaqlvxsxy7q2ryq"
 REGION = "il-jerusalem-1"
 KEY_FILE = r"C:\Users\kelle\Desktop\oracel_keys\kellerg2002@gmail.com_2023-08-23T11_46_39.769Z.pem"
+TRUSTED_METADATA = "trusted_metadata.txt"
+
 
 def read_docx():
     # Reads the docx file and returns it
@@ -49,6 +51,7 @@ def create_config():
     validate_config(config)
     return config
 
+
 def api_ai(translated, config):
     # Get the main key phrases
     ai_language_client = oci.ai_language.AIServiceLanguageClient(config)
@@ -61,51 +64,71 @@ def api_ai(translated, config):
                     text=f"{translated}",
                     language_code="en")]
         ))
-    print(batch_detect_language_key_phrases_response.data)
+    # JSON that contains a dict with all the main key phrases and its score
     return batch_detect_language_key_phrases_response.data
 
-def Classify_ai(data):
-    # classifys the main 5 key phrases
+
+def classify_ai(data):
+    # Classifies the main 5 key phrases
     data = str(data)
     json_dict = json.loads(data)
-    dict5 = json_dict['documents'][0]['key_phrases'][1:5]
-    print(dict5)
-    return dict5
+    dict4 = json_dict['documents'][0]['key_phrases'][1:5]
+    # Return the 4 first main phrases from the dict (he passes the first word because we do not like her)
+    return dict4
 
-def google_search(search_term, api_key, cse_id, **kwargs):
+
+def google_search( value, api_key, cse_id, **kwargs):
     """this function took me soo mach time to make you
-    need to create google api with google cloud and your own search engine
-    welcome to PANTHERSEARCH"""
+    need to create google api with Google cloud and your own search engine
+    welcome to PANTHERSEARCH and no I didn't want to use selenium"""
 
+    term = ""
+    for word in value:
+        term += str(word) + " "
+
+    # the combine of the 4 main phrases for the search
+    search_term = term
     service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
+    req = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    # Returns the request == customsearch#search, the url we are searching
+    return req['items']
 
 
-def google_api_trunslet(fulltext):
-    # translets the docx from hebrew to english
+def google_api_translate(fulltext):
+    # translates the docx from hebrew to english
     to_translate = fulltext
     translated = GoogleTranslator(source='auto', target='en').translate(to_translate)
     return translated
 
-def google_api_trunslet_revers(translated):
+
+def google_api_translate_revers(translated):
     to_translate = translated
     translated = GoogleTranslator(source='en', target='iw').translate(to_translate)
     print(translated)
+
+
 def main():
     my_api_key = "AIzaSyAfAAwfwQFVCKNne9WJrNixONncLSgDp38"
     my_cse_id = "3028683e43d81493a"
     fulltext = read_docx()
-    translated = google_api_trunslet(fulltext)
+    translated = google_api_translate(fulltext)
     config = create_config()
     data = api_ai(translated, config)
     key_phrases = Classify_ai(data)
-
-    results = google_search('"god is a woman" "thank you next" "7 rings"', my_api_key, my_cse_id, num=10)
+    value = [prase.get('text') for prase in key_phrases]
+    results = google_search(value, my_api_key, my_cse_id, num=10)
+    # result is a line the Google gives us in the search
     for result in results:
-        print(result)
+        with open(TRUSTED_METADATA, "r") as trusted_f:
+            tf = trusted_f.readlines()
+            # line is a line in TRUSTED_METADATA file
+            for line in tf:
+                # Check if the current display link from the google search is equal to a line in the TRUSTED_METADATA
+                if (result['displayLink'] in line):
+                    print(result['title'], result['link'])
+                else:
+                    None
 
-    #translated = google_api_trunslet_revers(translated)
 
 if __name__ == "__main__":
     main()
